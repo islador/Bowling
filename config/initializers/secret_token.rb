dca8a5150e2cf1e05b213bffdc31871eba926467
islador <luke.isla@gmail.com>
2014-01-15 10:29:24 -0800

    Initial commit for the Passare Bowling Scorecard code challenge. Using rails 3.2.15, had to update bundler itself to get around a json-1.8.1 install error.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BowlingScorecard::Application.config.secret_token = 'b7b99b908cf2786030a4805ba3eaeab0bbb4b64fe3dcaf15f84b05391b5566ffe5fd368236601ebdcf4b73590b41e839838ca78e62a88275eff5d171880cd7a3'
