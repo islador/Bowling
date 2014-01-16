509b2fc69aacc6a8a115b978ca3e4cb4ff045b19
islador <luke.isla@gmail.com>
2014-01-16 02:09:24 -0800

    Added necessary id's to the scorecard partials. Straightened out some CSS so the scorecards aren't so repulsive and updated play.html to retrieve the scorecards.

--- spec/features/play_page_spec.rb
+++ spec/features/play_page_spec.rb
@@ -9,23 +9,35 @@ describe "Play Game Page > " do
 		before(:each) do
 			visit games_new_path
 		end
-		it "fill out name, select team 1 and click 'Add Player' should populate team 1 roster", js: true do
+		it "Play page should have a game number display.", js: true do
 			fill_in 'player_name', :with => 'Jack'
 			choose 'team_1'
 			click_button 'Add Player'
-
-			#should have_selector("td[data-player-team='1']")
 			click_button 'Play'
 
 			should have_selector('h2', text: "Game Number: 1")
 		end
+
+		it "Play page should have data-game-id with the current game id.", js: true do
+			fill_in 'player_name', :with => 'Jack'
+			choose 'team_1'
+			click_button 'Add Player'
+			click_button 'Play'
+
+			should have_selector("h2[data-game-id='1']")
+		end
+
+		it "Play page should have one scorecard for each player", js: true do
+			fill_in 'player_name', :with => 'Jack'
+			choose 'team_1'
+			click_button 'Add Player'
+			fill_in 'player_name', :with => 'Jill'
+			choose 'team_2'
+			click_button 'Add Player'
+			click_button 'Play'
+
+			should have_selector("div#scorecard[data-player-id='1']")
+			should have_selector("div#scorecard[data-player-id='2']")
+		end
 	end
-
-	#it "should have a game ID in the top center" do
-		#get :play, :game_id => game.id
-
-	#	should have_selector('h2', text: "Game Number ")
-	#end
-
-
 end
