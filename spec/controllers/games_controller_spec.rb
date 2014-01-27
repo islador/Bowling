5902caefdb0ceac7a49437f2adae9151b05223f2
islador <luke.isla@gmail.com>
2014-01-26 16:43:12 -0800

    Swapped databasecleaner :each strategy from transaction to truncation. This has resulted in slower tests, but works with postgres to clean the db properly at each test.

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -5,14 +5,8 @@ describe GamesController do
 
 	describe "New" do
 		it "should create a new game called games" do
-			#@time_now = Time.new(2014, 1, 15, 2, 2, 2, "-08:00")
-			#Time.stub(:now).and_return(@time_now)
-
-			#game = FactoryGirl.create(:game)
-
 			get :new
 			expect(assigns(:game)).to_not be_nil
-			#be_a_new(Game)#.to be true #have_same_attributes_as(game)
 		end
 
 		it "should create a players array for the new game" do
