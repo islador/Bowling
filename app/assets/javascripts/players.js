66a83f5a82df072406acf0b96be34f321577eaef
islador <luke.isla@gmail.com>
2014-01-30 00:14:34 -0800

    Ironed out the bug, .val() is for form elements and thus wouldn't work on the div being injected into the dom. Swapping to .text() ironed out the bug. All play page specs are now green.

--- app/assets/javascripts/players.js
+++ app/assets/javascripts/players.js
@@ -12,10 +12,10 @@ $(document).ready(function(){
 		    	data: { player_id: player_id, 
 		    		throw_id: throw_id, 
 		    		score: score},
-		    	success: hide_id(this.id)
+		    	complete: hide_id(this.id)
 		    });
 		//});
-		//#alert(score + player_id + throw_id);
+		//alert(throw_id);
 	});
 
 	function hide_id(id){
@@ -24,9 +24,6 @@ $(document).ready(function(){
 		target_id = "score_select_" + (parseInt(element.attr("data-throw-id"))+1) + "_p" + element.attr("data-player-id")
 
 		previous_select = "score_select_" + (parseInt(element.attr("data-throw-id"))-1) + "_p" + element.attr("data-player-id")
-
-		//alert(previous_select)
-		alert($("#" + previous_select).val())
 
 		//Remove illegal scores from the second select in a frame if there isn't a strike and current frame isn't frame 20
 		if(parseInt(element.attr("data-throw-id")) != 20) {
@@ -44,10 +41,7 @@ $(document).ready(function(){
 		}
 
 		if(parseInt(element.attr("data-throw-id")) == 20) {
-			alert("hai")
-			//alert(element.val())
-			alert($("#" + previous_select).val())
-			if(parseInt(element.val()) + parseInt($("#" + previous_select).val()) < 10) {
+			if(parseInt(element.val()) + parseInt($("#" + previous_select).text()) < 10) {
 				$("#"+ target_id).hide();
 			}
 		}
@@ -75,6 +69,6 @@ $(document).ready(function(){
 		
 		var current = "#" + current_id
 		var score = $(current).val();
-		$("#outer_" + current_id).empty().append("<div id='" + current_id + "' data-score-value='" + score + "' data-player-id='' data-throw-id='' value='" + score + "'>" + score + "</div>");
+		$("#outer_" + current_id).empty().append("<div id='" + current_id + "' data-score-value='" + score + "'>" + score + "</div>");
 	}
 });
